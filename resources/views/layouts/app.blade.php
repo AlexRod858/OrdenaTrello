ff31454e32b2a920d361a43027e3e4e13a3cd292
AlexRod858 <oa.defrag@gmail.com>
2023-05-26 20:11:17 +0100

    Navbar corregido

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,9 +21,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
+                <a class="navbar-brand" href="{{ url('/home') }}">
+                    <img src="https://www.corralejo.info/images/ordenatech-servicios-informaticos-fuerteventura-fb.jpg" width="200" height="80" alt="">
+                  </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -31,9 +31,24 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
+                        @if (Auth::check() && Auth::user()->admin)
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('admin.proyectos.index') }}">{{ __('Proyectos') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('admin.tareas.index') }}">{{ __('Tareas') }}</a>
+                        </li>
+                    @else
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('proyectos.index') }}">{{ __('Proyectos') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('tareas.index') }}">{{ __('Tareas') }}</a>
+                        </li>
+                    @endif
+                    
                     </ul>
-
+                
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
