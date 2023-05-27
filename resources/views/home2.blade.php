4b95232ae936455872a57554477858c9908a62c9
AlexRod858 <oa.defrag@gmail.com>
2023-05-28 00:19:09 +0100

    Dashboard terminado, falta funcionalidad

--- resources/views/home2.blade.php
+++ resources/views/home2.blade.php
@@ -5,42 +5,32 @@
     {{-- <div id="example"></div> --}}
     <div class="row justify-content-center">
         {{--  --}}
-        <div class="col-md-4 p-5">
+        <div class="col-md-6 p-5">
             <div class="card text-white bg-dark border border-5" style="height: calc(60vh); border-color: rgb(245, 119, 35)!important;">
                 {{-- <div class="card-header">{{ __('Dashboard') }}</div> --}}
                 <div class="card-body d-flex flex-column align-items-center justify-content-center">
                     <div class="text-center bg-warning rounded-circle" style="height: 150px; width: 150px; background-color: rgb(245, 119, 35)!important; margin-top: -75px; display: flex; align-items: center; justify-content: center;">
                         <span style="font-size: 72px; font-weight: bold;">5</span>
                     </div>
-                    <h3 class="mt-3">PROYECTOS ABIERTOS</h3>
+                    <h3 class="mt-3">TAREAS PENDIENTES</h3>
                 </div>
             </div>
         </div>
         
         {{--  --}}
-        <div class="col-md-4 p-5">
+        <div class="col-md-6 p-5">
             <div class="card text-white bg-dark border border-5" style="height: calc(60vh); border-color: rgb(245, 119, 35)!important;">
                 {{-- <div class="card-header">{{ __('Dashboard') }}</div> --}}
                 <div class="card-body d-flex flex-column align-items-center justify-content-center">
                     <div class="text-center bg-warning rounded-circle" style="height: 150px; width: 150px; background-color: rgb(245, 119, 35)!important; margin-top: -75px; display: flex; align-items: center; justify-content: center;">
-                        <span style="font-size: 72px; font-weight: bold;">2</span>
+                        <span style="font-size: 72px; font-weight: bold;">21</span>
                     </div>
-                    <h3 class="text-center pt-1">TAREAS FINALIZADAS EN LAS ÚLTIMAS 24H</h3>
+                    <h3 class="text-center pt-1">TOTAL TAREAS FINALIZADAS</h3>
                 </div>
             </div>
         </div>
-        {{--  --}}
-        <div class="col-md-4 p-5">
-            <div class="card text-white bg-dark border border-5" style="height: calc(60vh); border-color: rgb(245, 119, 35)!important;">
-                {{-- <div class="card-header">{{ __('Dashboard') }}</div> --}}
-                <div class="card-body d-flex flex-column align-items-center justify-content-center">
-                    <div class="text-center bg-warning rounded-circle" style="height: 150px; width: 150px; background-color: rgb(245, 119, 35)!important; margin-top: -75px; display: flex; align-items: center; justify-content: center;">
-                        <span style="font-size: 72px; font-weight: bold;">4</span>
-                    </div>
-                    <h3 class="mt-3">TAREAS PENDIENTES</h3>
-                </div>
-            </div>
-        </div>
+
+
         {{--  --}}
     </div>
 </div>
